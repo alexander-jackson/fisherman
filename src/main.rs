ca1320da33ae167bc8a21205872a8d4f4eb1031f
Alexander Jackson <alexanderjackson@protonmail.com>
2021-02-19 09:14:28 +0000

    Swap to `actix-web` as a web framework

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
-use warp::Filter;
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+use crate::webhook::Webhook;
 
 #[macro_use]
 extern crate serde;
 
-pub mod git;
+mod git;
 mod logging;
 mod webhook;
 
-use crate::webhook::Webhook;
+async fn handle_webhook(webhook: web::Json<Webhook>) -> HttpResponse {
+    log::debug!("Webhook body: {:?}", &webhook);
 
-#[tokio::main]
-async fn main() {
+    if webhook.is_master_push() {
+        log::info!("Commits were pushed to `master` in this event");
+
+        // Pull the new changes
+        webhook.trigger_pull().expect("Failed to execute the pull.");
+
+        // Build the updated binary
+        webhook
+            .trigger_build()
+            .expect("Failed to rebuild the binary");
+
+        // Restart in `supervisor`
+        webhook
+            .trigger_restart()
+            .expect("Failed to restart the process");
+    }
+
+    HttpResponse::Ok().finish()
+}
+
+#[actix_rt::main]
+async fn main() -> actix_web::Result<()> {
     logging::setup_logger();
 
-    let webhook = warp::post().and(warp::body::json()).map(|body: Webhook| {
-        log::debug!("Webhook body: {:?}", &body);
+    let server = HttpServer::new(move || App::new().route("/", web::post().to(handle_webhook)))
+        .bind("127.0.0.1:5000")?
+        .run();
 
-        if body.is_master_push() {
-            log::info!("Commits were pushed to `master` in this event");
-            body.trigger_pull().expect("Failed to execute the pull.");
-            body.trigger_build().expect("Failed to rebuild the binary");
-            body.trigger_restart()
-                .expect("Failed to restart the process");
-        }
+    server.await?;
 
-        "Thanks for the update"
-    });
-
-    warp::serve(webhook).run(([127, 0, 0, 1], 5000)).await;
+    Ok(())
 }
